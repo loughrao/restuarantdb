ef82c1df7f42f01586d9566930faa93f70095221
loughrao <loughrao@oregonstate.edu>
2019-02-16 22:18:21 -0800

    Simple home page

---
+++ webInterface/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="card" v-for="(tab, i) in tabs" :key="i">
+      <div class="card-content">
+        <h2 class="red-text">{{ tab.title }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      tabs: [
+        {title: 'Menus', slug: 'menus'},
+        {title: 'Food', slug: 'food'},
+        {title: 'Employees', slug: 'employees'},
+        {title: 'Roles', slug: 'roles'},
+        {title: 'Customers', slug: 'Customers'},
+        {title: 'Orders', slug: 'Orders'}
+
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.home{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 15px;
+  margin-top: 60px;
+}
+.home h2{
+  font-size: 1.8em;
+  text-align: center;
+}
+</style>
---
+++ webInterface/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navbar">
+    <nav class ="nav-extended maroon darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title"> RestuarantDB</span>
+        </router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.navbar nav{
+  padding: 0 20px;
+}
+
+</style>
